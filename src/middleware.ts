4d0445e08b1297574030a5e2ee881cf1ee24592f
Souhardya20 <souhardyadeb2016@gmail.com>
2025-02-25 10:27:05 +0530

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
     const isPublic = path === '/login';
-    const token = request.cookies.get('token')?.value || '';   
+    const token = request.cookies.get('tokenAdmin')?.value || '';   
     if (isPublic && token) {
         return NextResponse.redirect(new URL('/', request.nextUrl));
     }
