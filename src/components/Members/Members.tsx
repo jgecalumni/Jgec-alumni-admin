e8a1f2119910c36d8e2e195f92adcac85fa818f5
Souhardya20 <souhardyadeb2016@gmail.com>
2025-04-16 18:30:15 +0530

    update

--- src/components/Members/Members.tsx
+++ src/components/Members/Members.tsx
@@ -192,7 +192,7 @@ const Members = () => {
 								disabled={page === totalPages}
 								className="px-4 py-2 bg-primary text-white rounded-md text-sm font-normal disabled:opacity-50 disabled:cursor-not-allowed flex items-center gap-1"
 							>
-								Next
+								Nex
 								<ArrowRight size={14} />
 							</button>
 						</div>
